7c1edf3fa0eb87b2561888b49813b4e131959a06
taco.chang <tacochang@telmediatech.com>
2023-03-21 14:34:16 +0800

    build(global.d.ts(demo)): add global type

---
+++ apps/demo/src/global.d.ts
@@ -0,0 +1,9 @@
+declare module '@alienfast/i18next-loader?relativePathAsNamespace=true!*' {
+  const contents: import('i18next').Resource;
+
+  export default contents;
+}
+
+declare const __WEBPACK_DEFINE__: {
+  LANGUAGES: string[];
+};

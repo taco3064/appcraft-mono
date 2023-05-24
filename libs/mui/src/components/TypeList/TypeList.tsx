3a11af59f3c162da0aaea37f6d4e307840456daf
LAPTOP-1DQ6UC5L\tabac <tabacotaco@gmail.com>
2023-05-25 01:15:46 +0800

    chore(type-list(@appcraft/mui)): remove TypeSubheader

--- libs/mui/src/components/TypeList/TypeList.tsx
+++ libs/mui/src/components/TypeList/TypeList.tsx
@@ -1,7 +1,14 @@
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
 import List from '@mui/material/List';
+import ListSubheader from '@mui/material/ListSubheader';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 import { TypeItem } from '../TypeItem';
-import { TypeSubheader } from '../TypeSubheader';
 import { usePropPath } from '../../contexts';
 import { usePropertyRouter, useTypeItems } from '../../hooks';
 import type { TypeListProps } from './TypeList.types';
@@ -23,23 +30,64 @@ export default function TypeList({
   return (
     <List
       subheader={
-        <TypeSubheader
-          breadcrumbs={breadcrumbs}
-          onBack={handleBack}
-          onAddElement={
-            !isModifiable
-              ? undefined
-              : () =>
-                  onChange(
-                    superior.type === 'arrayOf'
-                      ? [...((value as []) || []), null]
-                      : {
-                          ...value,
-                          [`key_${Object.keys(value || {}).length}`]: null,
-                        }
-                  )
-          }
-        />
+        <ListSubheader
+          component={Toolbar}
+          variant="dense"
+          sx={{
+            background: 'inherit',
+            gap: (theme) => theme.spacing(1),
+            minHeight: 0,
+          }}
+        >
+          {breadcrumbs.length > 0 && (
+            <IconButton size="small" onClick={() => handleBack()}>
+              <ChevronLeftIcon />
+            </IconButton>
+          )}
+
+          <Breadcrumbs separator="." style={{ marginRight: 'auto' }}>
+            {breadcrumbs.map(({ name, isArrayElement, isLast }, i) =>
+              isLast ? (
+                <Typography
+                  key={`${name}_${i}`}
+                  variant="subtitle1"
+                  color="secondary"
+                >
+                  {isArrayElement ? `[${name}]` : name}
+                </Typography>
+              ) : (
+                <Link
+                  key={`${name}_${i}`}
+                  component="button"
+                  underline="hover"
+                  variant="subtitle1"
+                  color="text.primary"
+                  onClick={() => handleBack(i)}
+                >
+                  {isArrayElement ? `[${name}]` : name}
+                </Link>
+              )
+            )}
+          </Breadcrumbs>
+
+          {breadcrumbs.length > 0 && isModifiable && (
+            <IconButton
+              size="small"
+              onClick={() =>
+                onChange(
+                  superior.type === 'arrayOf'
+                    ? [...((value as []) || []), null]
+                    : {
+                        ...value,
+                        [`key_${Object.keys(value || {}).length}`]: null,
+                      }
+                )
+              }
+            >
+              <PlaylistAddIcon />
+            </IconButton>
+          )}
+        </ListSubheader>
       }
     >
       {items.map(({ key, options, onItemRemove }) => (

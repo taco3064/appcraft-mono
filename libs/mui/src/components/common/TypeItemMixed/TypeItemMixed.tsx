ec9b22071d345b1e95ff308291c3ab997cfbd0a8
LAPTOP-1DQ6UC5L\tabac <tabacotaco@gmail.com>
2023-05-31 14:49:20 +0800

    refactor(@appcraft/mui): update MenuPopover as MenuDialog

--- libs/mui/src/components/common/TypeItemMixed/TypeItemMixed.tsx
+++ libs/mui/src/components/common/TypeItemMixed/TypeItemMixed.tsx
@@ -4,14 +4,13 @@ import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import IconButton from '@mui/material/IconButton';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
 import Tooltip from '@mui/material/Tooltip';
 import UndoIcon from '@mui/icons-material/Undo';
 import { useState } from 'react';
 
 import { GapTypography, TypeItemAction } from '../../../styles';
+import { MenuDialog } from '../MenuDialog';
 import { useDisplayPropName } from '../../../hooks';
 import { useFixedT, useMixedTypeMapping } from '../../../contexts';
 import type { TypeItemMixedProps } from './TypeItemMixed.types';
@@ -22,12 +21,11 @@ export default function TypeItemMixed({
   options,
   renderMatchedField,
 }: TypeItemMixedProps) {
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [open, setOpen] = useState(false);
   const [selected, setSelected] = useMixedTypeMapping(options.propName);
 
   const ct = useFixedT();
   const displayName = useDisplayPropName(options.propName);
-  const horizontal = selected ? 'right' : 'center';
   const matched = options.options?.find(({ text }) => text === selected);
 
   return (
@@ -43,17 +41,14 @@ export default function TypeItemMixed({
             </Tooltip>
 
             <Tooltip title={ct('btn-reset-type')}>
-              <IconButton
-                color="secondary"
-                onClick={(e) => setAnchorEl(e.currentTarget)}
-              >
+              <IconButton color="secondary" onClick={() => setOpen(true)}>
                 <PlaylistAddCheckIcon />
               </IconButton>
             </Tooltip>
           </>
         )
       ) : (
-        <ListItemButton onClick={(e) => setAnchorEl(e.currentTarget)}>
+        <ListItemButton onClick={() => setOpen(true)}>
           {selection}
 
           <ListItemText
@@ -82,42 +77,26 @@ export default function TypeItemMixed({
         </ListItemButton>
       )}
 
-      <Menu
-        PaperProps={{ elevation: 0 }}
-        open={Boolean(anchorEl)}
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'bottom', horizontal }}
-        transformOrigin={{ vertical: 'top', horizontal }}
-        onClose={() => setAnchorEl(null)}
-      >
-        {options.options
-          ?.sort(({ type: t1, text: p1 }, { type: t2, text: p2 }) => {
-            const s1 = `${t1}:${p1}`;
-            const s2 = `${t2}:${p2}`;
+      {Array.isArray(options.options) && (
+        <MenuDialog
+          value={selected}
+          open={open}
+          onChange={setSelected}
+          onClose={() => setOpen(false)}
+          options={options.options
+            ?.sort(({ type: t1, text: p1 }, { type: t2, text: p2 }) => {
+              const s1 = `${t1}:${p1}`;
+              const s2 = `${t2}:${p2}`;
 
-            return s1 < s2 ? -1 : s1 > s2 ? 1 : 0;
-          })
-          .map(({ type, text }, i) => (
-            <MenuItem
-              key={text}
-              selected={text === selected}
-              value={text}
-              onClick={() => {
-                setSelected(text);
-                setAnchorEl(null);
-              }}
-            >
-              <ListItemText
-                primaryTypographyProps={{
-                  fontWeight: 'bolder',
-                  textTransform: 'capitalize',
-                }}
-                primary={type}
-                secondary={text.replace(/import\s*\(.*?\)\s*;?\./g, '')}
-              />
-            </MenuItem>
-          ))}
-      </Menu>
+              return s1 < s2 ? -1 : s1 > s2 ? 1 : 0;
+            })
+            .map(({ type, text }) => ({
+              primary: type,
+              secondary: text.replace(/import\s*\(.*?\)\s*;?\./g, ''),
+              value: text,
+            }))}
+        />
+      )}
     </>
   );
 }
--- libs/mui/src/components/common/TypeItemNode/TypeItemNode.tsx
+++ libs/mui/src/components/common/TypeItemNode/TypeItemNode.tsx
@@ -3,8 +3,6 @@ import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import IconButton from '@mui/material/IconButton';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
 import Tooltip from '@mui/material/Tooltip';
 import UndoIcon from '@mui/icons-material/Undo';
@@ -12,6 +10,7 @@ import WidgetsIcon from '@mui/icons-material/Widgets';
 import { useState } from 'react';
 
 import { GapTypography, TypeItemAction } from '../../../styles';
+import { MenuDialog } from '../MenuDialog';
 import { useDisplayPropName } from '../../../hooks';
 import { useFixedT, useMixedTypeMapping } from '../../../contexts';
 import type { TypeItemNodeProps } from './TypeItemNode.types';
@@ -21,21 +20,15 @@ export default function TypeItemNode({
   selection,
   options,
 }: TypeItemNodeProps) {
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [open, setOpen] = useState(false);
   const [selected, setSelected] = useMixedTypeMapping(options.propName);
 
   const ct = useFixedT();
   const displayName = useDisplayPropName(options.propName);
-  const horizontal = selected ? 'right' : 'center';
-
-  const handleTypeSelect = (type: string) => {
-    setSelected(type);
-    setAnchorEl(null);
-  };
 
   return (
     <>
-      <ListItemButton onClick={(e) => setAnchorEl(e.currentTarget)}>
+      <ListItemButton onClick={() => setOpen(true)}>
         {selection}
 
         <ListItemText
@@ -67,10 +60,7 @@ export default function TypeItemNode({
             </Tooltip>
 
             <Tooltip title={ct('btn-reset-type')}>
-              <IconButton
-                color="secondary"
-                onClick={(e) => setAnchorEl(e.currentTarget)}
-              >
+              <IconButton color="secondary" onClick={() => setOpen(true)}>
                 <PlaylistAddCheckIcon />
               </IconButton>
             </Tooltip>
@@ -80,32 +70,17 @@ export default function TypeItemNode({
         )}
       </ListItemButton>
 
-      <Menu
-        PaperProps={{ elevation: 0 }}
-        open={Boolean(anchorEl)}
-        anchorEl={anchorEl}
-        anchorOrigin={{ vertical: 'bottom', horizontal }}
-        transformOrigin={{ vertical: 'top', horizontal }}
-        onClose={() => setAnchorEl(null)}
-      >
-        {['plain-text', 'widgets', 'array-map'].map((type) => (
-          <MenuItem
-            key={type}
-            selected={selected === type}
-            value={type}
-            onClick={() => handleTypeSelect(type)}
-          >
-            <ListItemText
-              primaryTypographyProps={{
-                fontWeight: 'bolder',
-                textTransform: 'capitalize',
-              }}
-              primary={ct(`ttl-node-${type}`)}
-              secondary={ct(`txt-node-${type}`)}
-            />
-          </MenuItem>
-        ))}
-      </Menu>
+      <MenuDialog
+        value={selected}
+        open={open}
+        onChange={setSelected}
+        onClose={() => setOpen(false)}
+        options={['plain-text', 'widgets', 'array-map'].map((type) => ({
+          primary: ct(`ttl-node-${type}`),
+          secondary: ct(`txt-node-${type}`),
+          value: type,
+        }))}
+      />
     </>
   );
 }

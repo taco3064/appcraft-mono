a812a66292554f664dd5c4b46bcef8fb1e819b38
taco3064 <tabacotaco@gmail.com>
2023-06-20 09:14:58 +0800

    fix(usehierarchyfilter): breadcrumbs doesn't need to contain the item

--- apps/appcraft/src/hooks/useHierarchyFilter/useHierarchyFilter.ts
+++ apps/appcraft/src/hooks/useHierarchyFilter/useHierarchyFilter.ts
@@ -18,15 +18,24 @@ const useHierarchyFilter: Types.HierarchyFilterHook = (category, itemId) => {
     keyword: query.keyword as string | undefined,
     superiors,
 
-    breadcrumbs: Object.keys(superiors).map((id, i, arr) => ({
-      text: superiors[id],
-      url: {
-        pathname: `/${category}`,
-        query: {
-          superiors: arr.slice(0, i + 1).join('-'),
-        },
-      },
-    })),
+    breadcrumbs: Object.keys(superiors).reduce(
+      (result, id, i, arr) =>
+        id === itemId
+          ? result
+          : [
+              ...result,
+              {
+                text: superiors[id],
+                url: {
+                  pathname: `/${category}`,
+                  query: {
+                    superiors: arr.slice(0, i + 1).join('-'),
+                  },
+                },
+              },
+            ],
+      []
+    ),
   };
 };
 

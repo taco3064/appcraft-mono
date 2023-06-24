a27d2da6a0e513f81289a42fa03104b0769c7901
taco3064 <tabacotaco@gmail.com>
2023-06-25 00:40:48 +0800

    feat(todos): add run-test button

--- apps/appcraft/src/pages/todos/index.tsx
+++ apps/appcraft/src/pages/todos/index.tsx
@@ -2,13 +2,14 @@ import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+import { CommonButton } from '~appcraft/components/common';
 import { HierarchyList } from '~appcraft/containers';
-import { PageContainer } from '~appcraft/styles';
+import { PageContainer, PlayTodoIcon } from '~appcraft/styles';
 import { useFixedT, useNodePickHandle } from '~appcraft/hooks';
 
 export default function Todos() {
   const { pathname } = useRouter();
-  const [nt] = useFixedT('nav');
+  const [nt, tt] = useFixedT('nav', 'todos');
 
   const [action, handleActionNodePick] = useNodePickHandle([
     'search',
@@ -37,6 +38,14 @@ export default function Todos() {
         category={pathname.replace(/^\//, '')}
         icon={AssignmentRoundedIcon}
         onActionNodePick={handleActionNodePick}
+        onItemActionRender={(data) => (
+          <CommonButton
+            btnVariant="icon"
+            icon={PlayTodoIcon}
+            text={tt('btn-run')}
+            onClick={() => console.log('test')}
+          />
+        )}
       />
     </PageContainer>
   );
--- apps/appcraft/src/styles/index.ts
+++ apps/appcraft/src/styles/index.ts
@@ -1,5 +1,6 @@
 export * from './MuiAlert';
 export * from './MuiContainer';
 export * from './MuiDrawer';
+export * from './MuiIcon';
 export * from './MuiLink';
 export * from './MuiListItemIcon';

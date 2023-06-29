5df923749bef1d9fe4659ce010ceb524de927934
taco3064 <tabacotaco@gmail.com>
2023-06-29 12:39:45 +0800

    style(@appcraft/mui): update TypeItemAction styles

--- libs/mui/src/components/WidgetNode/WidgetNode.tsx
+++ libs/mui/src/components/WidgetNode/WidgetNode.tsx
@@ -5,13 +5,12 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import IconButton from '@mui/material/IconButton';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
 import ListItemText from '@mui/material/ListItemText';
 import { useState } from 'react';
 import type * as Appcraft from '@appcraft/types';
 
 import * as Hooks from '../../hooks';
-import { IconTipButton } from '../../styles';
+import { IconTipButton, TypeItemAction } from '../../styles';
 import type { WidgetNodeProps } from './WidgetNode.types';
 
 type MixedWidget = Appcraft.PlainTextWidget & Appcraft.NodeWidget;
@@ -61,14 +60,14 @@ export default function WidgetNode<I extends Appcraft.WidgetOptions>({
             : { primary: ct('ttl-node-plain-text'), secondary: content })}
         />
 
-        <ListItemSecondaryAction onClick={(e) => e.stopPropagation()}>
+        <TypeItemAction onClick={(e) => e.stopPropagation()}>
           <IconTipButton
             title={ct('btn-remove-widget')}
             onClick={() => onRemove(item)}
           >
             <CloseIcon />
           </IconTipButton>
-        </ListItemSecondaryAction>
+        </TypeItemAction>
       </ListItemButton>
 
       {structures.length > 0 && (
@@ -78,7 +77,12 @@ export default function WidgetNode<I extends Appcraft.WidgetOptions>({
               <ListItemIcon />
 
               <ListItemText
-                primaryTypographyProps={{ variant: 'subtitle2' }}
+                primaryTypographyProps={{
+                  variant: 'subtitle2',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'nowrap',
+                }}
                 secondaryTypographyProps={{ variant: 'caption' }}
                 primary={path}
                 secondary={type}

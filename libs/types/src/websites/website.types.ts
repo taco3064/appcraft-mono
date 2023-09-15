2c9179fa87e7c91b4946f3f78541d3d145fd9375
taco3064 <tabacotaco@gmail.com>
2023-09-15 13:49:34 +0800

    fix(@appcraft/types): update the type's content of Website

--- libs/types/src/websites/website.types.ts
+++ libs/types/src/websites/website.types.ts
@@ -1,3 +1,18 @@
 export interface Website {
   theme: string;
+  nav: {
+    position: 'top' | 'left';
+  };
+  pages: {
+    id: string; //* Page Config ID
+    subTitle: string; //* 副標題，與 Website Title(Hierarchy Name) 顯示在網頁標題列
+    pathname: string; //* 網址路徑
+    isNavItem: boolean; //* 是否為導覽列項目
+    routes?: Website['pages']; //* 子頁面
+    links?: {
+      layout: string; //* Layout ID
+      todoPath: string; //* 觸發事項路徑
+      to: string; //* 跳轉路徑
+    }[];
+  }[];
 }

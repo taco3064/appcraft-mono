266c384c53ce4570eb449a1584b2ea96a495fbec
taco3064 <tabacotaco@gmail.com>
2023-10-11 17:13:41 +0800

    fix(apps/appcraft): fixed the duplicated title warning

--- apps/appcraft/src/containers/AppLayout/AppLayout.tsx
+++ apps/appcraft/src/containers/AppLayout/AppLayout.tsx
@@ -1,6 +1,5 @@
 import Button from '@mui/material/Button';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
-import Head from 'next/head';
 import NextLink from 'next/link';
 import NoSsr from '@mui/material/NoSsr';
 import Slide from '@mui/material/Slide';
@@ -34,76 +33,70 @@ export default function AppLayout({ children }: AppLayoutProps) {
   }, [query.pathname]);
 
   return (
-    <>
-      <Head>
-        <title>Appcraft | {wt('ttl-preview')}</title>
-      </Head>
+    <NoSsr>
+      <ThemeProvider themeid={config?.website.theme}>
+        <Style.MuiSnackbarProvider>
+          {error || !config ? (
+            <Slide in direction="down" timeout={{ enter: 1200 }}>
+              <Style.MaxWidthAlert
+                maxWidth="sm"
+                variant="outlined"
+                severity="error"
+                msgVariant="h6"
+                icon={<ErrorOutlineIcon fontSize="large" />}
+                sx={{ marginTop: 6 }}
+                action={
+                  query.websiteid && (
+                    <Button
+                      {...{ replace: true as never }}
+                      fullWidth
+                      variant="text"
+                      size="large"
+                      href={`/websites/detail?id=${query.websiteid}`}
+                      LinkComponent={NextLink}
+                    >
+                      {wt('btn-to-setting')}
+                    </Button>
+                  )
+                }
+              >
+                {wt('msg-invalid-preview')}
+              </Style.MaxWidthAlert>
+            </Slide>
+          ) : (
+            <WebsiteConfigProvider config={config}>
+              <AppHeader
+                title={{ text: config.title, href: '/' }}
+                onMenuToggle={(e) => {
+                  e.stopPropagation();
+                  setOpen(!open);
+                }}
+              />
 
-      <NoSsr>
-        <ThemeProvider themeid={config?.website.theme}>
-          <Style.MuiSnackbarProvider>
-            {error || !config ? (
-              <Slide in direction="down" timeout={{ enter: 1200 }}>
-                <Style.MaxWidthAlert
-                  maxWidth="sm"
-                  variant="outlined"
-                  severity="error"
-                  msgVariant="h6"
-                  icon={<ErrorOutlineIcon fontSize="large" />}
-                  sx={{ marginTop: 6 }}
-                  action={
-                    query.websiteid && (
-                      <Button
-                        {...{ replace: true as never }}
-                        fullWidth
-                        variant="text"
-                        size="large"
-                        href={`/websites/detail?id=${query.websiteid}`}
-                        LinkComponent={NextLink}
-                      >
-                        {wt('btn-to-setting')}
-                      </Button>
-                    )
-                  }
-                >
-                  {wt('msg-invalid-preview')}
-                </Style.MaxWidthAlert>
-              </Slide>
-            ) : (
-              <WebsiteConfigProvider config={config}>
-                <AppHeader
-                  title={{ text: config.title, href: '/' }}
-                  onMenuToggle={(e) => {
-                    e.stopPropagation();
-                    setOpen(!open);
-                  }}
-                />
+              <WebsiteNavMenu
+                key={config.website.navAnchor}
+                basename={`/app/${config.token}`}
+                open={open}
+                options={config.website}
+              />
 
-                <WebsiteNavMenu
-                  key={config.website.navAnchor}
-                  basename={`/app/${config.token}`}
-                  open={open}
-                  options={config.website}
-                />
-
-                <Style.MainContainer
-                  maxWidth={false}
-                  className="app"
-                  component="main"
-                  onClick={() => setOpen(false)}
-                  sx={(theme) => ({
-                    height: `calc(${height} - ${theme.spacing(
-                      open && config.website.navAnchor === 'top' ? 16 : 9
-                    )})`,
-                  })}
-                >
-                  {children}
-                </Style.MainContainer>
-              </WebsiteConfigProvider>
-            )}
-          </Style.MuiSnackbarProvider>
-        </ThemeProvider>
-      </NoSsr>
-    </>
+              <Style.MainContainer
+                maxWidth={false}
+                className="app"
+                component="main"
+                onClick={() => setOpen(false)}
+                sx={(theme) => ({
+                  height: `calc(${height} - ${theme.spacing(
+                    open && config.website.navAnchor === 'top' ? 16 : 9
+                  )})`,
+                })}
+              >
+                {children}
+              </Style.MainContainer>
+            </WebsiteConfigProvider>
+          )}
+        </Style.MuiSnackbarProvider>
+      </ThemeProvider>
+    </NoSsr>
   );
 }

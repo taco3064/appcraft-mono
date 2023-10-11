c84572045b3f569d7b9ae6779e86fd931e1e1b73
taco3064 <tabacotaco@gmail.com>
2023-10-11 13:38:37 +0800

    fix(apps/appcraft): support to render the page content in website app

--- apps/appcraft/src/pages/app/[token]/[...pathname].tsx
+++ apps/appcraft/src/pages/app/[token]/[...pathname].tsx
@@ -1,14 +1,30 @@
 import Head from 'next/head';
+import { CraftedRenderer } from '@appcraft/exhibitor';
+import { useQuery } from '@tanstack/react-query';
+import { useTheme } from '@mui/material/styles';
 
 import { AppLayout, Breadcrumbs } from '~appcraft/containers';
 import { PageContainer } from '~appcraft/styles';
+import { findConfig } from '~appcraft/services';
+import { GRID_LAYOUT, useCraftsmanFetch } from '~appcraft/hooks';
 import { useWebsiteConfig } from '~appcraft/contexts';
 import { withPerPageLayout } from '~appcraft/hocs';
+import type { PageData } from '~appcraft/hooks';
 
 export default withPerPageLayout(AppLayout, function WebsitePage() {
   const { config, routes } = useWebsiteConfig();
   const { token, title, website } = config;
+
+  const fetchHandles = useCraftsmanFetch();
+  const theme = useTheme();
   const page = routes[routes.length - 1];
+
+  const { data } = useQuery({
+    enabled: Boolean(page?.pageid),
+    queryKey: [page?.pageid],
+    queryFn: findConfig<PageData>,
+    refetchOnWindowFocus: false,
+  });
 
   return (
     <PageContainer
@@ -39,6 +55,26 @@ export default withPerPageLayout(AppLayout, function WebsitePage() {
           })
         }
       />
+
+      {data?.content && (
+        <CraftedRenderer
+          elevation={1}
+          options={data.content.layouts}
+          onFetchData={fetchHandles.data}
+          onFetchWrapper={fetchHandles.wrapper}
+          onReady={data.content.readyTodos}
+          GridLayoutProps={{
+            autoSize: true,
+            cols: GRID_LAYOUT.COLS,
+            mins: GRID_LAYOUT.MINS,
+            breakpoints: Object.fromEntries(
+              Object.entries(theme.breakpoints.values).sort(
+                ([, w1], [, w2]) => w2 - w1
+              )
+            ),
+          }}
+        />
+      )}
     </PageContainer>
   );
 });
